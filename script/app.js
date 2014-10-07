8dbb171660f39c9d1d3c41f400b4354572fb0406
Dariel Noel <darielnoel@gmail.com>
2014-10-06 23:23:08 -0400

    Commenting code.

--- script/app.js
+++ script/app.js
@@ -2,14 +2,26 @@
   $(function() { //on DOM ready
     var App = {};
 
-
+    /**
+     * Init function, the app starting point
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     */
     App.init = function(){
+      // retrieve data from server and execute a callback after
+      // finish that
       App.retrieveData(function(data){
         App.renderUI(data);
         App.bindUI();
       });
     };
 
+    /**
+     * Render the application data
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @param  {[type]}   data items JSON
+     */
     App.renderUI = function(data){
         var itemsPerColumn,
           itemListSize = data.length,
@@ -20,8 +32,8 @@
           actualColumnItemIndex = 0,
           templateSource;
 
-      //Debemos dividir entre la cantidad de columnas la cantidad
-      //de items para que quede lo mas equitativo posible
+      // we divide by the number of columns the number of items
+      // to make it as fair as possible more
       itemsPerColumn = Math.floor(itemListSize / App.getColumnCount());
 
       $verticalSlider = $('#app').find('.slideshow-vertical');
@@ -37,12 +49,23 @@
       }
     };
 
+    /**
+     * Render an item
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @param  {model}   item Can be video or image model representation
+     */
     App.renderItem = function(item){
       var compiledTemplateFunction;
       compiledTemplateFunction = App.getCompiledTemplate(item.type);
       return compiledTemplateFunction(item);
     };
 
+    /**
+     * Put all event listeners
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     */
     App.bindUI = function(){
       $('#app').imagesLoaded(function () {
         App.unLockInterfaceAction();
@@ -55,14 +78,8 @@
       });
     };
 
-    App.playVideo = function(){
-      $('.video-slide .vid').each(function () {
-          $(this).get(0).play();
-      });
-    };
-
     /**
-     * Init the scroll
+     * Init the infinite scroll loop
      * @author Dariel Noel <darielnoel@gmail.com>
      * @since  2014-10-04
      */
@@ -90,13 +107,35 @@
         $($(".vert")[actualColumn]).height($(window).height() - 80);
         actualColumn++;
       }
-
     };
 
+    /**
+     * Hide the init load layer
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     */
     App.unLockInterfaceAction = function(){
       $('#loading-icon').hide();
     };
 
+    /**
+     * Play all videos
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     */
+    App.playVideo = function(){
+      $('.video-slide .vid').each(function () {
+          $(this).get(0).play();
+      });
+    };
+
+    /**
+     * Retrieve model data from backend
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
     App.retrieveData = function(callback){
       var data = [];
 
@@ -106,10 +145,22 @@
       callback(data);
     };
 
+    /**
+     * Return the window resolution
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @return {[type]}   [description]
+     */
     App.getResolution = function(){
       return $(window).width();
     };
 
+    /**
+     * Get the columns counts based on screen resolution
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @return {[type]}   [description]
+     */
     App.getColumnCount = function(){
       var columnCount = 4;
       //Debemos saber cuantas columnas se van a renderear
@@ -119,6 +170,13 @@
       return columnCount;
     };
 
+    /**
+     * Return a compiled template
+     * @author Dariel Noel <darielnoel@gmail.com>
+     * @since  2014-10-06
+     * @param  {[type]}   type [description]
+     * @return {[type]}        [description]
+     */
     App.getCompiledTemplate = function(type){
       var templateType = "#item-image-template",
           template;
